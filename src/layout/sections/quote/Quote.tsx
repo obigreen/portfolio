c2134ab2984932788f8f37092b47f95466041b71
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 17:54:48 +0300

    added styles

--- src/layout/sections/quote/Quote.tsx
+++ src/layout/sections/quote/Quote.tsx
@@ -1,35 +1,76 @@
 import React from 'react';
 import styled from "styled-components";
+import {Container} from "../../../styles/Container";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {theme} from "../../../styles/Theme";
+import arrow from "../../../assets/img/pseudoelements/arrows.svg"
 
 export const Quote = () => {
     return (
         <StyledQuote>
-            <StyledBlockquote>
-                <p>With great power comes great electricity bill</p>
-                <footer>- Dr. Who</footer>
-            </StyledBlockquote>
+            <Container>
+                <FlexWrapper justify={'center'}>
+                    <StyledBlockquote>
+                        <QuoteText>With great power comes great electricity bill</QuoteText>
+                        <Author>- Dr. Who</Author>
+                    </StyledBlockquote>
+                </FlexWrapper>
+
+            </Container>
+
         </StyledQuote>
     );
 };
 
 const StyledQuote = styled.section`
-  min-height: 30vh;
-  display: flex;
-  justify-content: center;
+    padding-bottom: 74px;
 `
 
 const StyledBlockquote = styled.blockquote`
   display: flex;
   align-items: end;
   flex-direction: column;
-  
-  padding: 32px;
-
-
-  //font-family: var(--font-family);
   font-weight: 500;
   font-size: 24px;
-  color: #fff;
+  color: ${theme.color.mainFont};
+`
+
+const QuoteText = styled.p`
+  padding: 32px;
+  outline: 1px solid ${theme.color.main};
+  position: relative;
+  
+  &::before {
+    content: "";
+    background-image: url(${arrow});
+    background-repeat: no-repeat;
+    width: 42px;
+    height: 29px;
+    
+    position: absolute;
+    top: -15px;
+    left: 11px;
+  }
+`
+
+const Author = styled.footer`
+  padding: 16px;
+  margin-top: 1px;
+  outline: 1px solid ${theme.color.main};
+  position: relative;
+
+  &::before {
+    content: "";
+    background-image: url(${arrow});
+    background-repeat: no-repeat;
+    width: 42px;
+    height: 29px;
+
+    position: absolute;
+    top: -15px;
+    right: 11px;
+  }
+  
 `
 
 
