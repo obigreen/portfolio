b53e140b2fd22b62dd513c46b3a9ba6a1be18068
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 17:54:40 +0300

    added styles

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -4,46 +4,53 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Project} from "./Project";
 
-import projectOne from '../../../assets/img/projectOne.jpg'
-import projectTwo from '../../../assets/img/projectTwo.jpg'
-import projectThree from '../../../assets/img/projectThree.jpg'
+import projectOne from '../../../assets/img/projectOne.png'
+import projectTwo from '../../../assets/img/projectTwo.png'
+import projectThree from '../../../assets/img/projectThree.png'
+import {Container} from "../../../styles/Container";
 
 
 export const Projects = () => {
     return (
         <StyledProjects>
-            <FlexWrapper align={'center'} justify={'space-between'}>
 
-                <SectionTitle>projects</SectionTitle>
+            <Container>
+                {/*для ссылки на другую страницу*/}
+                <FlexWrapper>
+                    <SectionTitle>projects</SectionTitle>
+                </FlexWrapper>
 
 
-            </FlexWrapper>
+
+                <FlexWrapper align={'start'} justify={'space-between'}>
+                    <Project
+                        src={projectOne}
+                        technologies={'HTML SCSS Python Flask'}
+                        title={'ChertNodes'}
+                        text={'Minecraft servers hosting'}
+                        buttonText={'Live <~>'}/>
+                    <Project
+                        src={projectTwo}
+                        technologies={'React Express Discord.js Node.js HTML SCSS Python Flask'}
+                        title={'ProtectX'}
+                        text={'Discord anti-crash bot'}
+                        buttonText={'Live <~>'}/>
+                    <Project
+                        src={projectThree}
+                        technologies={'CSS Express Node.js'}
+                        title={'Kahoot Answers Viewer'}
+                        text={'Get answers to your kahoot quiz'}
+                        buttonText={'Live <~>'}/>
+                </FlexWrapper>
+            </Container>
 
 
-            <FlexWrapper align={'start'} justify={'space-around'}>
-                <Project
-                    src={projectOne}
-                    technologies={'HTML SCSS Python Flask'}
-                    title={'ChertNodes'}
-                    text={'Minecraft servers hosting'}
-                    buttonText={'Live <~>'}/>
-                <Project
-                    src={projectTwo}
-                    technologies={'React Express Discord.js Node.js HTML SCSS Python Flask'}
-                    title={'ProtectX'}
-                    text={'Discord anti-crash bot'}
-                    buttonText={'Live <~>'}/>
-                <Project
-                    src={projectThree}
-                    technologies={'CSS Express Node.js'}
-                    title={'Kahoot Answers Viewer'}
-                    text={'Get answers to your kahoot quiz'}
-                    buttonText={'Live <~>'}/>
-            </FlexWrapper>
         </StyledProjects>
     );
 };
 
 const StyledProjects = styled.section`
-
+${SectionTitle} {
+  margin-bottom: 48px;
+}
 `
