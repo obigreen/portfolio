f9b8fbce2c27896a0c6e8fd0a5296189eb7fd02d
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 22:54:34 +0300

    tried to do it through props + added styles

--- src/layout/sections/aboutme/AboutMe.tsx
+++ src/layout/sections/aboutme/AboutMe.tsx
@@ -5,36 +5,48 @@ import {SectionText} from "../../../components/SectionText";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import BottomFhoto from "../../../assets/img/photo-bottom.png"
 import {Button} from "../../../components/Button";
+import {Container} from "../../../styles/Container";
 
 export const AboutMe = () => {
     return (
         <StyledAboutMe>
-            <SectionTitle>about-me</SectionTitle>
-            <FlexWrapper align={'center'}>
-                <FlexWrapper direction={'column'} justify={'space-beetwen'} gap={'30px'}>
-                    <SectionText>
-                        Hello, i’m Elias!
-                    </SectionText>
-                    <SectionText>
-                        I’m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from
-                        scratch and raise them into modern user-friendly web experiences.
-                    </SectionText>
-                    <SectionText>
-                        Transforming my creativity and knowledge into a websites has been my passion for over a year. I have
-                        been helping various clients to establish their presence online. I always strive to learn about the
-                        newest technologies and frameworks.
-                    </SectionText>
-                    <Button type={"submit"}>Read more</Button>
+            <Container>
+                <FlexWrapper justify={'space-between'}>
+                    <FlexWrapper direction={'column'} justify={'space-beetwen'} gap={'30px'}>
+                        <SectionTitle>about-me</SectionTitle>
+                        <SectionText>
+                            Hello, i’m Elias!
+                        </SectionText>
+                        <SectionText>
+                            I’m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from
+                            scratch and raise them into modern user-friendly web experiences.
+                        </SectionText>
+                        <SectionText>
+                            Transforming my creativity and knowledge into a websites has been my passion for over a year. I have
+                            been helping various clients to establish their presence online. I always strive to learn about the
+                            newest technologies and frameworks.
+                        </SectionText>
+                        <Button><Link href={''}>Read more -&gt;</Link></Button>
+                    </FlexWrapper>
+                    <img src={BottomFhoto} alt=""/>
                 </FlexWrapper>
-                <img src={BottomFhoto} alt=""/>
-            </FlexWrapper>
-
-
-
+            </Container>
         </StyledAboutMe>
     );
 };
 
 const StyledAboutMe = styled.section`
-    
+  ${FlexWrapper} > ${FlexWrapper} {
+    max-width: 515px;
+    width: 100%;
+  }
+
+  ${SectionTitle} {
+    margin-bottom: -7px;
+  }
+
+  padding-bottom: 112px;
+`
+const Link = styled.a`
+
 `
