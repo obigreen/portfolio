522af69b49ad59a21757500c113d89e23acd31ea
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 17:34:29 +0300

    transfer styles to GlobalStyles.ts

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,7 @@
-import styled, {css} from "styled-components";
+import styled from "styled-components";
 
-type ButtonTypeProps = {
-    width?: 'buttonWidth'
-}
 
-export const Button = styled.button<ButtonTypeProps>`
-  border: 1px solid #c778dd;
-  padding: 8px 16px;
-  background: transparent;
-  //font-family: var(--font-family);
-  font-weight: 500;
-  font-size: 16px;
-  color: #fff;
-  ${props => props.width === 'buttonWidth'&& css<ButtonTypeProps>`
-    
-    max-width: 171px;
-  `}
+export const Button = styled.button`
+ 
+ 
 `
