b0303a2d5198c3841fef666b3e662cb5d3df0976
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 17:50:39 +0300

    added styles for Project.tsx

--- src/layout/sections/projects/Project.tsx
+++ src/layout/sections/projects/Project.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import {SectionText} from "../../../components/SectionText";
 import {Button} from "../../../components/Button";
 import {TechnologiesMenu} from "../../../components/TechnologiesMenu";
+import {theme} from "../../../styles/Theme";
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 type StyledWorkPropsType = {
     title: string
@@ -16,21 +18,35 @@ type StyledWorkPropsType = {
 export const Project = (props: StyledWorkPropsType) => {
     return (
         <SyledWork>
-            <img src={props.src} alt=""/>
+            <FlexWrapper>
+                <img src={props.src} alt=""/>
+            </FlexWrapper>
+
             <TechnologiesMenu>{props.technologies}</TechnologiesMenu>
-            <Title>{props.title}</Title>
-            <SectionText>{props.text}</SectionText>
-            <Button>{props.buttonText}</Button>
+            <InfoProjectWrapper>
+                <Title>{props.title}</Title>
+                <SectionText margin={'cardTextMargin'}>{props.text}</SectionText>
+                <Button>{props.buttonText}</Button>
+            </InfoProjectWrapper>
+
         </SyledWork>
     );
 };
 
+const InfoProjectWrapper = styled.div`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+`
 
 const SyledWork = styled.div`
   max-width: 330px;
   width: 100%;
+  outline: 1px solid ${theme.color.main};
 `
 
 const Title = styled.h3`
-
+  font-weight: 500;
+  font-size: 24px;
+  color: ${theme.color.mainFont};
 `
