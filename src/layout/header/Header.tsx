b0c669ba635ea95cca3efcf244d27d8a17bb3950
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 17:41:49 +0300

    Structure Header.tsx

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,38 +2,35 @@ import React from 'react';
 import styled from "styled-components";
 import {Menu} from "../../components/menu/Menu";
 import {Logo} from "../../components/logo/Logo";
+import {Container} from "../../styles/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {Lang} from "./lang/Lang";
 
 const item = ["home", "works", "about-me", "contacts"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={item}/>
-            {/*<FlexWrapper>*/}
-            {/*    <LanguageMenu>*/}
-            {/*        EN*/}
-            {/*    </LanguageMenu>*/}
-            {/*    <Icon iconId={'arrow-down'}/>*/}
-            {/*</FlexWrapper>*/}
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <FlexWrapper>
+                        <Menu menuItems={item}/>
+                        <Lang/>
+                    </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 
-// const LanguageMenu = styled.span`
-//   //font-family: var(--font-family);
-//   font-weight: 600;
-//   font-size: 16px;
-//   color: #abb2bf;
-//   margin-right: 4px;
-// `
-
-
 const StyledHeader = styled.header`
   padding: 32px 0 8px 0;
-  display: flex;
-  justify-content: space-between;
+  position: fixed;
+  z-index: 999;
+  background-color: rgba(40, 44, 51, 0.89);
+  width: 100%;
 `
 
 
