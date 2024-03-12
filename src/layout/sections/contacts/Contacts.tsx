ad121918feaf1bedd15b1db9dcb838f28e3e2bc9
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-12 22:55:58 +0300

    stylized

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -4,18 +4,20 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {SectionText} from "../../../components/SectionText";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Contact} from "./Contact";
+import {Container} from "../../../styles/Container";
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-            <SectionTitle>contacts</SectionTitle>
-            <FlexWrapper justify={'space-between'}>
-                <SectionText>I’m interested in freelance opportunities. However, if you have other request or question,
-                    don’t hesitate to contact me
-                </SectionText>
-                <Contact/>
-            </FlexWrapper>
-
+            <Container>
+                <SectionTitle>contacts</SectionTitle>
+                <FlexWrapper justify={'space-between'}>
+                    <SectionText>I’m interested in freelance opportunities. However, if you have other request or question,
+                        don’t hesitate to contact me
+                    </SectionText>
+                    <Contact/>
+                </FlexWrapper>
+            </Container>
 
         </StyledContacts>
     );
@@ -23,5 +25,15 @@ export const Contacts = () => {
 
 
 export const StyledContacts = styled.section`
+  ${SectionTitle} {
+    margin-bottom: 45px;
+  }
+
+  ${SectionText} {
+    max-width: 505px;
+    width: 100%;
+  }
+  
+  margin-bottom: 145px;
 
 `
