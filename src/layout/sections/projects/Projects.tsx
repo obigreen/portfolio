ede06fbfec3a5e954b3f79a0bdc96321908a562f
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-18 19:06:14 +0300

    added color animation + before to section

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -8,6 +8,8 @@ import projectOne from '../../../assets/img/projectOne.png'
 import projectTwo from '../../../assets/img/projectTwo.png'
 import projectThree from '../../../assets/img/projectThree.png'
 import {Container} from "../../../styles/Container";
+import {AccentAnimationCol, Rotate} from "../../../components/AccentAnimation";
+import {theme} from "../../../styles/Theme";
 
 
 export const Projects = () => {
@@ -16,8 +18,8 @@ export const Projects = () => {
 
             <Container>
                 {/*для ссылки на другую страницу*/}
-                <FlexWrapper justify={'space-between'} align={'center'}>
-                    <SectionTitle>projects</SectionTitle>
+                <FlexWrapper justify={'space-between'} align={'center'} margin={'0 0 48px 0'}>
+                    <SectionTitle width={'285%'}>projects</SectionTitle>
                     <Link href={''}>View all ~~&gt;</Link>
                 </FlexWrapper>
 
@@ -51,13 +53,26 @@ export const Projects = () => {
 };
 
 const StyledProjects = styled.section`
-  ${SectionTitle} {
-    margin-bottom: 48px;
-  }
-
   padding-bottom: 106px;
+  position: relative;
+  overflow-x: hidden;
+
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 155px;
+    height: 155px;
+    border: 1px solid ${theme.color.main};
+    background-color: transparent;
+    right: calc(50% - 830px);
+    top: 40%;
+    animation: ${Rotate} 16s linear infinite;
+  }
+  
 `
 
 const Link = styled.a`
 display: inline-block;
+  animation: ${AccentAnimationCol} 12s linear infinite alternate;
 `
