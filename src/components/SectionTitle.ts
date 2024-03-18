008e8b219e67959fcfd56a5db7dfa160648ea154
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-18 19:07:25 +0300

    pumped up the headlines

---
+++ src/components/SectionTitle.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import {theme} from "../styles/Theme";
+import {AccentAnimationBg, AccentAnimationCol} from "./AccentAnimation";
+
+
+type SectionTitleAfterWidthType = {
+    maxWidth?: string
+    width?: string
+}
+
+export const SectionTitle = styled.h3<SectionTitleAfterWidthType>`
+  font-weight: 500;
+  font-size: 32px;
+  color: ${theme.color.mainFont};
+  display: inline-block;
+  position: relative;
+  
+  &::before {
+    content: '#';
+    animation: ${AccentAnimationCol} 12s linear infinite alternate;
+  }
+  
+  &::after {
+    content: '';
+    position: absolute;
+    animation: ${AccentAnimationBg} 12s linear infinite alternate;
+    top: 50%;
+    left: 120%;
+    transform: translateY(-50%);
+    width: ${props => props.width || "100%"};
+    height: 1px;
+  }
+`
