2c22b2870d41fe24883e7e7f3abbdc3b1cd3082a
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-18 18:52:47 +0300

    adjusted the size of the logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -5,9 +5,9 @@ import {FlexWrapper} from "../FlexWrapper";
 import {theme} from "../../styles/Theme";
 export const Logo = () => {
     return (
-        <a href="" >
-            <FlexWrapper>
-                <Icon iconId={'logo'}/>
+        <a href="">
+            <FlexWrapper align={'center'}>
+                <Icon iconId={'logo'} width={'16'} height={'16'} wiewBox={'0 0 16 16'}/>
                 <LogoText>Elias</LogoText>
             </FlexWrapper>
 
