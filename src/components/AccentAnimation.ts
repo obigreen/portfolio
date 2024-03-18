1c18e58c3d8648c1aec2ef098d407423e70882a4
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-18 18:37:48 +0300

    created an accent color animation

---
+++ src/components/AccentAnimation.ts
@@ -0,0 +1,151 @@
+import {keyframes} from "styled-components";
+
+export const AccentAnimationBg = keyframes`
+  0% {
+    background-color: #07F2FF;
+  }
+  10% {
+    background-color: #0DF3A8;
+  }
+  20% {
+    background-color: #01F35B;
+  }
+  30% {
+    background-color: #F3D35C;
+  }
+  40% {
+    background-color: #FF7C70;
+  }
+  50% {
+    background-color: #FC6771;
+  }
+  60% {
+    background-color: #FF4FC2;
+  }
+  70% {
+    background-color: #C862FA;
+  }
+  80% {
+    background-color: #6BA5FF;
+  }
+  90% {
+    background-color: #01EFFE;
+  }
+  100% {
+    background-color: #07F2FF;
+  }
+`
+export const AccentAnimationCol = keyframes`
+  0% {
+    color: #07F2FF;
+  }
+  10% {
+    color: #0DF3A8;
+  }
+  20% {
+    color: #01F35B;
+  }
+  30% {
+    color: #F3D35C;
+  }
+  40% {
+    color: #FF7C70;
+  }
+  50% {
+    color: #FC6771;
+  }
+  60% {
+    color: #FF4FC2;
+  }
+  70% {
+    color: #C862FA;
+  }
+  80% {
+    color: #6BA5FF;
+  }
+  90% {
+    color: #01EFFE;
+  }
+  100% {
+    color: #07F2FF;
+  }
+`
+export const AccentAnimationBorder = keyframes`
+  0% {
+    border-color: #07F2FF;
+  }
+  10% {
+    border-color: #0DF3A8;
+  }
+  20% {
+    border-color: #01F35B;
+  }
+  30% {
+    border-color: #F3D35C;
+  }
+  40% {
+    border-color: #FF7C70;
+  }
+  50% {
+    border-color: #FC6771;
+  }
+  60% {
+    border-color: #FF4FC2;
+  }
+  70% {
+    border-color: #C862FA;
+  }
+  80% {
+    border-color: #6BA5FF;
+  }
+  90% {
+    border-color: #01EFFE;
+  }
+  100% {
+    border-color: #07F2FF;
+  }
+`
+export const AccentAnimationFill = keyframes`
+  0% {
+    fill: #07F2FF;
+  }
+  10% {
+    fill: #0DF3A8;
+  }
+  20% {
+    fill: #01F35B;
+  }
+  30% {
+    fill: #F3D35C;
+  }
+  40% {
+    fill: #FF7C70;
+  }
+  50% {
+    fill: #FC6771;
+  }
+  60% {
+    fill: #FF4FC2;
+  }
+  70% {
+    fill: #C862FA;
+  }
+  80% {
+    fill: #6BA5FF;
+  }
+  90% {
+    fill: #01EFFE;
+  }
+  100% {
+    fill: #07F2FF;
+  }
+`
+
+export const Rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }  
+  100% {
+    transform: rotate(360deg);
+  }
+`
