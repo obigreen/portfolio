348a32180d3a65b5a1cab16fe537b2e1a7bb4e8f
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-18 19:07:51 +0300

    remove

--- src/components/SectionTitle.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import {theme} from "../styles/Theme";
-
-export const SectionTitle = styled.h3`
-  font-weight: 500;
-  font-size: 32px;
-  color: ${theme.color.mainFont};
-  padding-left: 20px;
-`
