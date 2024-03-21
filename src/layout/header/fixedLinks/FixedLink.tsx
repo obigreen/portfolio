b01c76f88476e37961cbb57f904d8e521aab6448
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-21 12:45:30 +0300

    created MediaLinks.tsx

--- src/layout/header/fixedLinks/FixedLink.tsx
+++ src/layout/header/fixedLinks/FixedLink.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import styled from "styled-components";
-import {Icon} from "../../../components/icon/Icon";
 import {theme} from "../../../styles/Theme";
+import {MediaLinks} from "../../../components/mediaLinks/MediaLinks";
 
 export const FixedLink = () => {
     return (
         <FixedLinkStyled>
             <Line/>
-            <a href="">
-                <Icon iconId={'github'} width={'32'} height={'32'} wiewBox={'0 0 32 32'}/>
-            </a>
-            <a href="">
-                <Icon iconId={'figma'} width={'32'} height={'32'} wiewBox={'0 0 32 32'}/>
-            </a>
-            <a href="">
-                <Icon iconId={'discord'} width={'32'} height={'32'} wiewBox={'0 0 32 32'}/>
-            </a>
+            <MediaLinks/>
         </FixedLinkStyled>
     );
 };
 
-const FixedLinkStyled = styled.div`
+export const FixedLinkStyled = styled.div`
   position: fixed;
   display: flex;
   flex-direction: column;
@@ -29,6 +21,11 @@ const FixedLinkStyled = styled.div`
   top: -32px;
   background-color: rgba(40, 44, 51, 0.89);
   padding: 0 5px 5px 5px;
+  
+  @media ${theme.media.fixedLinks} {
+    display: none;
+  }
+  
 `
 const Line = styled.div`
   width: 1px;
