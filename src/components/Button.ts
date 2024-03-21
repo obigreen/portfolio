c401c1fbe9678d26640d2cb23fccbfee8b697316
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-21 12:42:56 +0300

    export styles for button from GlobalStyles.ts in Button.ts

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,7 +1,18 @@
 import styled from "styled-components";
+import {AccentAnimationBorder} from "../styles/AccentAnimation";
+import {theme} from "../styles/Theme";
 
 
 export const Button = styled.button`
-
+  font-family: "Fira Code", sans-serif;
+  background: transparent;
+  border: 1px solid transparent;
+  animation: ${AccentAnimationBorder} 12s linear infinite alternate;
+  padding: 8px 16px;
+  font-weight: 500;
+  font-size: 16px;
+  color: ${theme.color.mainFont};
+  cursor: pointer;
+  max-width: max-content;
 
 `
