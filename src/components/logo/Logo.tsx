c594d3b5c8271d29680275d43d04155e6112663f
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-21 12:48:06 +0300

    added styles for <a> in Logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,19 +3,31 @@ import {Icon} from "../icon/Icon";
 import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper";
 import {theme} from "../../styles/Theme";
+
+
+
 export const Logo = () => {
     return (
-        <a href="">
+        <LogoLinkWrapp href={''}>
+
             <FlexWrapper align={'center'}>
                 <Icon iconId={'logo'} width={'16'} height={'16'} wiewBox={'0 0 16 16'}/>
                 <LogoText>Elias</LogoText>
             </FlexWrapper>
 
-        </a>
+        </LogoLinkWrapp>
+
+
     );
 };
 
-
+const LogoLinkWrapp = styled.a`
+  position: relative;
+  z-index: 9999;
+  display: flex;
+  align-items: center;
+  height: 30px;
+`
 
 const LogoText = styled.span`
   font-weight: 700;
@@ -23,3 +35,5 @@ const LogoText = styled.span`
   color: ${theme.color.mainFont};
   margin-left: 8px;
 `
+
+
