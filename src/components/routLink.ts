7ac8c1d62c7d8e50a8ada4740f6036eaf8bdf7d9
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-27 12:59:29 +0300

    create component routLink.ts

---
+++ src/components/routLink.ts
@@ -0,0 +1,7 @@
+import {AccentAnimationCol} from "../styles/AccentAnimation";
+import styled from "styled-components";
+
+export const RoutLink = styled.a`
+  display: inline-block;
+  animation: ${AccentAnimationCol} 12s linear infinite alternate;
+`
