89038bc91d00a23bd536b5f1cb379db453f56834
Sergey Budyakin <youshtuchka@gmail.com>
2024-04-23 21:32:17 +0300

    add pointer for <a

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,33 +2,34 @@ import {createGlobalStyle} from "styled-components";
 import {theme} from "./Theme";
 
 export const GlobalStyles = createGlobalStyle`
-  *,
-  *::before,
-  *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-  body {
-    background: ${theme.color.backround};
-    font-family: "Fira Code", sans-serif;
-  }
+    body {
+        background: ${theme.color.backround};
+        font-family: "Fira Code", sans-serif;
+    }
 
-  ul {
-    list-style: none;
-  }
+    ul {
+        list-style: none;
+    }
 
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+    a {
+        color: inherit;
+        text-decoration: none;
+        cursor: pointer;
+    }
 
-  button {
-    background-color: unset;
-    border: none;
-    cursor: pointer;
-  }
+    button {
+        background-color: unset;
+        border: none;
+        cursor: pointer;
+    }
 
 `
 
